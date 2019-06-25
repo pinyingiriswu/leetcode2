7a5551a85c79ec5a56976abea8b8e6b6a56c54be
Pin-Ying Wu <pinying2777@gmail.com>
2019-06-24 21:11:57 -0700

    day 1 problems

---
+++ d1/d1_237_delete_node_in_a_linked_list.java
@@ -0,0 +1,15 @@
+public class d1_237_delete_node_in_a_linked_list {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode(int x) { val = x; }
+     * }
+     */
+    public void deleteNode(ListNode node) {
+        if (node == null) return;
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
