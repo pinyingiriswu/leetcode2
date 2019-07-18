6b2e10fd6af69069db60377839a6d807eb829a8a
Pin-Ying Wu <pinying2777@gmail.com>
2019-07-17 23:12:59 -0700

    graph

--- 4-Graph/d24/d24_127_word_ladder.java
+++ 4-Graph/d24/d24_127_word_ladder.java
@@ -1,33 +1,31 @@
 public class d24_127_word_ladder {
+
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        if (beginWord.equals(endWord)) return 1;
+        HashSet<String> set = new HashSet<>(wordList);
+        Queue<String> q = new LinkedList<>();
+        int count = 2;
+        q.add(beginWord);
 
-        public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-            if (beginWord.equals(endWord)) return 1;
-            HashSet<String> set = new HashSet<>(wordList);
-            Queue<String> q = new LinkedList<>();
-            int count = 2;
-            q.add(beginWord);
-
-            while(!q.isEmpty()){
-                int size = q.size();
-                for(int i = 0; i < size; i++){
-                    String top = q.poll();
-                    for(int j = 0; j < top.length(); j++){
-                        StringBuilder sb = new StringBuilder(top);
-                        for(char c = 'a'; c <= 'z'; c++){
-                            sb.setCharAt(j, c);
-                            String s = sb.toString();
-                            if(set.contains(s)){
-                                if(s.equals(endWord)) return count;
-                                q.add(s);
-                                set.remove(s);
-                            }
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                String top = q.poll();
+                for(int j = 0; j < top.length(); j++){
+                    StringBuilder sb = new StringBuilder(top);
+                    for(char c = 'a'; c <= 'z'; c++){
+                        sb.setCharAt(j, c);
+                        String s = sb.toString();
+                        if(set.contains(s)){
+                            if(s.equals(endWord)) return count;
+                            q.add(s);
+                            set.remove(s);
                         }
                     }
                 }
-                count++;
             }
-            return 0;
+            count++;
         }
+        return 0;
     }
 }
